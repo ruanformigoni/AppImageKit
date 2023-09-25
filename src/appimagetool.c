317bd4d16b1d500c6a9bd047c032c38c9ec0b17c
Ruan E. Formigoni <ruanformigoni@gmail.com>
2023-09-24 23:19:47 -0300

    Zstd support

--- src/appimagetool.c
+++ src/appimagetool.c
@@ -166,11 +166,11 @@ int sfs_mksquashfs(char *source, char *destination, int offset) {
         bool use_xz = strcmp(sqfs_comp, "xz") >= 0;
 
         int i = 0;
-#ifndef AUXILIARY_FILES_DESTINATION
-        args[i++] = "mksquashfs";
-#else
-        args[i++] = pathToMksquashfs;
-#endif
+// #ifndef AUXILIARY_FILES_DESTINATION
+         args[i++] = "mksquashfs";
+// #else
+//         args[i++] = pathToMksquashfs;
+// #endif
         args[i++] = source;
         args[i++] = destination;
         args[i++] = "-offset";
@@ -234,13 +234,13 @@ int sfs_mksquashfs(char *source, char *destination, int offset) {
             printf("\n");
         }
 
-#ifndef AUXILIARY_FILES_DESTINATION
-        execvp("mksquashfs", args);
-        perror("execvp(\"mksquashfs\") failed");
-#else
-        execvp(pathToMksquashfs, args);
-        fprintf(stderr, "execvp(\"%s\") failed: %s\n", pathToMksquashfs, strerror(errno));
-#endif
+// #ifndef AUXILIARY_FILES_DESTINATION
+         execvp("mksquashfs", args);
+         perror("execvp(\"mksquashfs\") failed");
+// #else
+//         execvp(pathToMksquashfs, args);
+//         fprintf(stderr, "execvp(\"%s\") failed: %s\n", pathToMksquashfs, strerror(errno));
+// #endif
         return -1; // exec never returns
     }
     return 0;
@@ -609,8 +609,8 @@ main (int argc, char *argv[])
     if (showVersionOnly)
         exit(0);
 
-    if(!((0 == strcmp(sqfs_comp, "gzip")) || (0 ==strcmp(sqfs_comp, "xz"))))
-        die("Only gzip (faster execution, larger files) and xz (slower execution, smaller files) compression is supported at the moment. Let us know if there are reasons for more, should be easy to add. You could help the project by doing some systematic size/performance measurements. Watch for size, execution speed, and zsync delta size.");
+    if(!((0 == strcmp(sqfs_comp, "gzip")) || (0 ==strcmp(sqfs_comp, "xz")) || (0 ==strcmp(sqfs_comp, "zstd"))))
+        die("Only gzip (faster execution, larger files), xz (slower execution, smaller files) and zstd compression is supported at the moment. Let us know if there are reasons for more, should be easy to add. You could help the project by doing some systematic size/performance measurements. Watch for size, execution speed, and zsync delta size.");
     /* Check for dependencies here. Better fail early if they are not present. */
     if(! g_find_program_in_path ("file"))
         die("file command is missing but required, please install it");
